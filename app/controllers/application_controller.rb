4230d1dbf2f39e442280598f4e9f821bf9a81819
Alejandro Villegas <neopilatos@gmail.com>
2017-10-26 12:07:50 -0500

    Rest Petitions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :log_request
+  
+    private
+      def log_request
+        puts "Ha iniciado una petición"
+      end
 end
